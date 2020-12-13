848754713b6ebd1fb2df867b2da13e55428df9ec
NotSoFearfulSymmetry <sapowersschaub@madisoncollege.edu>
2020-12-13 06:53:58 -0600

    full test coverage for user DAO and character DAO

--- src/main/java/controller/DeleteCharacter.java
+++ src/main/java/controller/DeleteCharacter.java
@@ -1,43 +1,43 @@
-package controller;
-
-import entity.Character;
-import persistence.GenericDao;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import util.DaoFactory;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.List;
-
-@WebServlet(name = "DeleteCharacter", urlPatterns = { "/deleteCharacter" } )
-
-public class DeleteCharacter extends HttpServlet {
-
-    private final Logger logger = LogManager.getLogger(this.getClass());
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        GenericDao<Character> dao = DaoFactory.createDao(Character.class);
-        int id = ; //TODO: get from viewCharacters.jsp
-        Character character = dao.getByPropertyEqual("id", id);
-        dao.delete(character);
-        req.setAttribute("character", character);
-        logger.debug("Sending back the character/s..." + character);
-
-        RequestDispatcher dispatcher = req.getRequestDispatcher("/viewCharacters" +
-                ".jsp");
-        dispatcher.forward(req, resp);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doGet(req, resp);
-    }
-}
+//package controller;
+//
+//import entity.Character;
+//import persistence.GenericDao;
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
+//import util.DaoFactory;
+//
+//import javax.servlet.RequestDispatcher;
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//import java.util.List;
+//
+//@WebServlet(name = "DeleteCharacter", urlPatterns = { "/deleteCharacter" } )
+//
+//public class DeleteCharacter extends HttpServlet {
+//
+//    private final Logger logger = LogManager.getLogger(this.getClass());
+//
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+//            throws ServletException, IOException {
+//        GenericDao<Character> dao = DaoFactory.createDao(Character.class);
+//        int id = ; //TODO: get from viewCharacters.jsp
+//        Character character = dao.getByPropertyEqual("id", id);
+//        dao.delete(character);
+//        req.setAttribute("character", character);
+//        logger.debug("Sending back the character/s..." + character);
+//
+//        RequestDispatcher dispatcher = req.getRequestDispatcher("/viewCharacters" +
+//                ".jsp");
+//        dispatcher.forward(req, resp);
+//    }
+//
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        doGet(req, resp);
+//    }
+//}
--- target/test-classes/setupTestDatabase.sql
+++ target/test-classes/setupTestDatabase.sql
@@ -12,8 +12,8 @@ INSERT INTO dnd_character (id, name, race, gender, class_and_level, strength, de
 INSERT INTO dnd_character (id, name, race, gender, class_and_level, strength, dexterity, constitution, intelligence, wisdom, charisma, hit_points, skills, feats, class_features, equipment, description, background, second_user_access, second_username, user_id) VALUES (2, 'Miri the Farsighted', 'elf', 'female', 'Ranger 5', 12, 6, 12, 11, 11, 10, 34, 'Skills', 'Feats', 'Class Features', 'Equipment', 'Description', 'Background', TRUE, 'iamthedragon42', 2);
 INSERT INTO dnd_character (id, name, race, gender, class_and_level, strength, dexterity, constitution, intelligence, wisdom, charisma, hit_points, skills, feats, class_features, equipment, description, background, second_user_access, second_username, user_id) VALUES (3, 'Flint Firefist', 'dwarf', 'male', 'Monk 3', 13, 13, 13, 10, 10, 9, 22, 'Skills', 'Feats', 'Class Features', 'Equipment', 'Description', 'Background', FALSE, NULL, 3);
 
-INSERT INTO role (id, role_name, username, user_id) VALUES (1, 'admin', 'ElfLord', 1);
-INSERT INTO role (id, role_name, username, user_id) VALUES (2, 'user', 'iamthedragon42', 3);
-INSERT INTO role (id, role_name, username, user_id) VALUES (3, 'user', 'RogueOneThird', 2);
+INSERT INTO role (id, username, role_name, user_id) VALUES (1, 'ElfLord', 'admin', 1);
+INSERT INTO role (id, username, role_name, user_id) VALUES (2, 'iamthedragon42', 'user', 3);
+INSERT INTO role (id, username, role_name, user_id) VALUES (3, 'RogueOneThird', 'user', 2);
 
 SET foreign_key_checks = 1;
